03b2a7f93785fd098e4d52b09231226a50f06a07
sivavadrevu5-maker <sivavadrevu5@gmail.com>
2025-11-27 15:21:03 +0530

    Update and rename sample to productRoutes.js

---
+++ ecommerce/backend/routes/productRoutes.js
@@ -0,0 +1,14 @@
+import express from "express";
+import {
+  getProducts,
+  getProductById,
+  createProduct
+} from "../controllers/productController.js";
+
+const router = express.Router();
+
+router.get("/", getProducts);
+router.get("/:id", getProductById);
+router.post("/", createProduct);
+
+export default router;
